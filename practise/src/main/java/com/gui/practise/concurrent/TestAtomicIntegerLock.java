7ca8db5ed7bc89597dea18bdb49516a235404245
williamjava <wuhoujian@126.com>
2018-09-17 09:21:10 +0800

    添加Lock和synchronized的测试

---
+++ practise/src/main/java/com/gui/practise/concurrent/TestAtomicIntegerLock.java
@@ -0,0 +1,85 @@
+package com.gui.practise.concurrent;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class TestAtomicIntegerLock {
+    private static int synValue;
+    public static void main(String[] args) {
+        int threadNum = 10;
+        int maxValue = 1000000000;
+        testSync(threadNum, maxValue);
+        testLocck(threadNum, maxValue);
+    }
+    
+    /**
+     * test lock
+     * @param threadNum
+     * @param maxValue
+     */
+    public static void testSync(int threadNum, int maxValue) {
+        Thread[] t = new Thread[threadNum];
+        Long begin = System.nanoTime();
+        for (int i = 0; i < threadNum; i++) {
+            Lock locks = new ReentrantLock();
+            synValue = 0;
+            t[i] = new Thread(() -> {
+                for (int j = 0; j < maxValue; j++) {
+                    locks.lock();
+                    try {
+                        synValue++;
+                    } finally {
+                        locks.unlock();
+                    }
+                }
+            });
+        }
+        for (int i = 0; i < threadNum; i++) {
+            t[i].start();
+        }
+        
+        //main线程等待前面开启的所有线程结束
+        for (int i = 0; i < threadNum; i++) {
+            try {
+                t[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("使用lock所花费的时间为：" + (System.nanoTime() - begin));
+    }
+    
+    /**
+     * test synchronized
+     * @param threadNum
+     * @param maxValue
+     */
+    public static void testLocck(int threadNum, int maxValue) {
+        int[] lock = new int[0];
+        Long begin = System.nanoTime();
+        Thread[] t = new Thread[threadNum];
+        for (int i = 0; i < threadNum; i++) {
+            synValue = 0;
+            t[i] = new Thread(() -> {
+                for (int j = 0; j < maxValue; j++) {
+                    synchronized(lock) {
+                        ++synValue;
+                    }
+                }
+            });
+        }
+        for (int i = 0; i < threadNum; i++) {
+            t[i].start();
+        }
+        
+        //main线程等待前面开启的所有线程结束
+        for (int i = 0; i < threadNum; i++) {
+            try {
+                t[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("使用synchronized所花费的时间为：" + (System.nanoTime() - begin));
+    }
+}
--- practise/src/main/java/com/gui/practise/design_model/factory/factorymethod1/ext1/NvWa.java
+++ practise/src/main/java/com/gui/practise/design_model/factory/factorymethod1/ext1/NvWa.java
@@ -15,20 +15,18 @@ import com.gui.practise.design_model.factory.factorymethod1.YellowHuman;
 public class NvWa {
 
 	public static void main(String[] args) {
-		AbstractHumanFactory humanFactory = new HumanFactory();
-
 		System.out.println("女娲开始创建黄种人了。。。。");
-		YellowHuman yellowHuman = humanFactory.createHuman(YellowHuman.class);
+		YellowHuman yellowHuman = HumanFactory.createHuman(YellowHuman.class);
 		yellowHuman.getColor();
 		yellowHuman.talk();
 
 		System.out.println("女娲开始创建白种人了。。。");
-		WhiteHuman whiteHuman = humanFactory.createHuman(WhiteHuman.class);
+		WhiteHuman whiteHuman = HumanFactory.createHuman(WhiteHuman.class);
 		whiteHuman.getColor();
 		whiteHuman.talk();
 
 		System.out.println("女娲开始创建黑种人了。。。");
-		BlackHuman blackHuman = humanFactory.createHuman(BlackHuman.class);
+		BlackHuman blackHuman = HumanFactory.createHuman(BlackHuman.class);
 		blackHuman.getColor();
 		blackHuman.talk();
 	}
