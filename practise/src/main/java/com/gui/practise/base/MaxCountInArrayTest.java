8046279954e892e9a01545b7268ca5a83e21581a
wuhoujian <wuhoujian@126.com>
2019-04-11 16:24:05 +0800

    获取一个数组中出现次数最多的元素

---
+++ practise/src/main/java/com/gui/practise/base/MaxCountInArrayTest.java
@@ -0,0 +1,30 @@
+package com.gui.practise.base;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 找出一个数组中，出现次数最多的元素
+ */
+public class MaxCountInArrayTest {
+    public static void main(String[] args) {
+        Integer [] nums = {1,2,3,2,2,2,5,4,2};
+        Map<Integer, Integer> sourceMap = new HashMap<>();
+        for (int i =0;i<nums.length;i++) {
+            if (sourceMap.get(nums[i]) == null) {
+                sourceMap.put(nums[i], 1);
+            } else {
+                int temp = sourceMap.get(nums[i]);
+                sourceMap.put(nums[i], temp +1);
+            }
+        }
+
+        int maxCount = Collections.max(sourceMap.values());
+        for (Map.Entry<Integer, Integer> entry: sourceMap.entrySet()) {
+            if (entry.getValue() == maxCount) {
+                System.out.println(entry.getKey());
+            }
+        }
+    }
+}
