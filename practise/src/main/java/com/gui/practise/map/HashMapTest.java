98d7e2bced31ee4a8ba18b1cbd313fbb5bd3270a
wuhoujian <wuhoujian@126.com>
2019-04-28 17:23:10 +0800

    map测试

--- practise/src/main/java/com/gui/practise/map/HashMapTest.java
+++ practise/src/main/java/com/gui/practise/map/HashMapTest.java
@@ -1,7 +1,9 @@
 package com.gui.practise.map;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * HashMap测试类
@@ -14,8 +16,26 @@ public class HashMapTest {
         map.put("city", "Hangzhou");
         map.put("job", "Engineer");
 
-        map.values().stream().forEach(v -> {
-            System.out.println(v);
-        });
+        //getEntries(map);
+
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            //getEntries(map);
+            //System.out.println("value: " + entry.getValue());
+            System.out.println("666666");
+        }
+    }
+
+    private static void getEntries(HashMap<String, Object> map) {
+        Set<Map.Entry<String, String>> entries = null;
+        try {
+            Field field = map.getClass().getDeclaredField("entrySet");
+            field.setAccessible(true);
+            entries = (Set<Map.Entry<String, String>>)field.get(map);
+            System.out.println("entries:" + entries);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
