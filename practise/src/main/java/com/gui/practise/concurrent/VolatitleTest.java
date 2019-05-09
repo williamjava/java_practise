dde37272b2d878a4a7d1697207d82eb26f955f0a
wuhoujian <wuhoujian@126.com>
2019-05-09 16:16:58 +0800

    1.增加ArrayList经典测试类； 2.增加volatitle测试类；

---
+++ practise/src/main/java/com/gui/practise/concurrent/VolatitleTest.java
@@ -0,0 +1,36 @@
+package com.gui.practise.concurrent;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class VolatitleTest {
+    private static  AtomicInteger a = new AtomicInteger(0);
+    //    public static volatile int a = 0;
+
+    public static void add() {
+        //        a++;
+        a.getAndIncrement();
+    }
+
+    private static final int size = 20;
+
+    public static void main(String[] args) {
+        Thread[] threads = new Thread[size];
+
+        for (int i = 0; i < size; i++) {
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 10000; i++) {
+                        add();
+                    }
+                }
+            });
+            threads[i].start();
+        }
+
+        while (Thread.activeCount() > 1)
+            Thread.yield();
+
+        System.out.println(a);
+    }
+}
